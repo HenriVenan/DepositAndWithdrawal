eea08f9ff5305a73e2829e5b931d90f3acea386c
Bino <abnerhbraga@outlook.pt>
2024-04-15 09:11:05 -0300

    Finished

---
+++ EmployeeSalary/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    internal class Employee
+    {
+        private int numeroConta {  get; }
+        private string titular { get; }
+        private double saldo { get; set; }
+
+        public Employee(int numeroConta, string titular, double saldo)
+        {
+            this.numeroConta = numeroConta;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public Employee(int numeroConta, string titular)
+        {
+            this.numeroConta = numeroConta;
+            this.titular = titular;
+        }
+
+        public void Deposito(double valorDeposito)
+        {
+            saldo += valorDeposito;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            if (valorSaque > saldo)  
+                saldo = 0; 
+            else 
+                saldo -= valorSaque; 
+        }
+
+        public override string ToString()
+        {
+            return 
+                "Conta: " + numeroConta +
+                ", Titular: " + titular + 
+                ", Saldo: " + saldo;
+        }
+    }
+}
---
+++ EmployeeSalary/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace EmployeeSalary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial: ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+
+            
+
+            if (depositoInicial == 's')
+            {
+                Console.Write("Entre com o valor do saldo inicial: ");
+                double valorSaldo = double.Parse(Console.ReadLine());
+
+                Employee conta = new Employee(numeroConta, nomeTitular, valorSaldo);
+                Console.WriteLine("\n" + conta);
+
+                Console.Write("Entre com o valor para deposito: ");
+                double valorDeposito = double.Parse(Console.ReadLine());
+                conta.Deposito(valorDeposito);
+
+                Console.WriteLine("\n" + conta);
+
+                Console.Write("Entre com o valor para saque: ");
+                double valorSaque = double.Parse(Console.ReadLine());
+                conta.Saque(valorSaque);
+
+                Console.WriteLine("\n" + conta);
+            } else if (depositoInicial == 'n')
+            {
+                Employee conta = new Employee(numeroConta, nomeTitular);
+                Console.WriteLine("\n" + conta);
+
+                Console.Write("Entre com o valor para deposito: ");
+                double valorDeposito = double.Parse(Console.ReadLine());
+                conta.Deposito(valorDeposito);
+
+                Console.WriteLine("\n" + conta);
+
+                Console.Write("Entre com o valor para saque: ");
+                double valorSaque = double.Parse(Console.ReadLine());
+                conta.Saque(valorSaque);
+
+                Console.WriteLine("\n" + conta);
+            } else
+            {
+                Console.WriteLine("\nResposta Inválida, tente novamente");
+            }
+
+            
+
+            
+        }
+    }
+}
